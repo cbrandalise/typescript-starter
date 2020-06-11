d9538942bf989a315bd504fcb4b6bd9be2c693f3
Christian Brandalise <christian.brandalise@tradingtechnologies.com>
2020-06-10 20:07:42 -0500

    minimum required package

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  title: 'Hello, World'
+});
+
+const cleanWebpackPlugin = new CleanWebpackPlugin();
+
+const config = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  plugins: [
+    htmlPlugin,
+    cleanWebpackPlugin
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+module.exports = config;
