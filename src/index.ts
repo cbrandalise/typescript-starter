f3e847529cf83e21e81cc6283d59e3e00a4decc9
Christian Brandalise <christian.brandalise@tradingtechnologies.com>
2020-06-10 20:34:35 -0500

    adds anchor div and queries anchor div in sample index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 class App {
+    public anchorDiv: HTMLElement;
     constructor() {
         console.log("HELLO WORLD");
+        this.anchorDiv = document.getElementById('app');
+        console.log(this.anchorDiv);
     }
 }
 
